9b4ca8f07107d59ba63970031b051738fbdabe7d
Bereket Urgessa <bereketadugna15@gmail.com>
2022-01-29 22:44:57 -0500

    Add components - basic app stracture

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 
-export default function About() {
+export default function Footer() {
   return (
     <div>
-      <h1>About</h1>
+      <h1>Footer</h1>
     </div>
   );
 }
--- src/components/Portfolio/Portfolio.jsx
+++ src/components/Portfolio/Portfolio.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 
-export default function About() {
+export default function Portfolio() {
   return (
     <div>
-      <h1>About</h1>
+      <h1>Portfolio</h1>
     </div>
   );
 }
