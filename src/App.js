ec9c2c49b33f45f43f0e1e7e2adc0bc907ea2e43
Bereket Urgessa <bereketadugna15@gmail.com>
2022-05-30 03:26:05 -0400

    add Skills component to app.js

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import logo from "./logo.svg";
 import "./App.css";
 import Header from "./components/Header/Header";
 import About from "./components/About/About";
+import Skills from "./components/Skills/Skills";
 import Contact from "./components/Contact/Contact";
 import Portfolio from "./components/Portfolio/Portfolio";
 import Footer from "./components/Footer/Footer";
@@ -12,6 +13,7 @@ function App() {
     <div className="App">
       <Header />
       <About />
+      <Skills />
       <Portfolio />
       <Contact />
       <Footer />
